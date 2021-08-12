05cea05b58b68dbac9ebf3bc30dd51c6a90ad413
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-12 10:15:03 -0400

    Add files via upload

---
+++ Milestones 2/144_ms2.c
@@ -0,0 +1,454 @@
+/* Name: Arshia Rahim
+Milestone: 2
+Section: SJJ
+Date: 2017/03/29
+Student_ID: 112142161
+Student_user: arahim5
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Define statements:
+
+#define LINEAR 1
+#define FORM 0
+// Constant TAX value:
+
+const double TAX = 0.13;
+// Item structure:
+
+struct Item {
+	double price;
+	int sku;
+	int isTaxed;
+	int quantity;
+	int minQuantity;
+	char name[21];
+};
+// Function Prototypes Milestone 1:
+
+void welcome(void);
+void printTitle(void);
+void printFooter(double gTotal);
+void flushKeyboard(void);
+void pause(void);
+int getInt(void);
+double getDouble(void);
+int getIntLimited(int lowerLimit, int upperLimit);
+double getDoubleLimited(double lowerLimit, double upperLimit);
+
+//interface
+
+int yes(void);
+void GroceryInventorySystem(void);
+int menu(void);
+
+// Function Prototypes Milesone 2:
+
+double totalAfterTax(struct Item item);
+int isLowQuantity(struct Item item);
+struct Item itemEntry(int sku);
+void displayItem(struct Item item, int linear);
+void listItems(const struct Item item[], int noOfItems);
+int locateItem(const struct Item item[], int noOfRecs, int sku, int *index);
+
+// Function Implementations Milestone 1:
+
+void welcome(void)
+{
+	printf("---=== Grocery Inventory System ===---\n\n");
+}
+
+void printTitle(void)
+{
+	printf("Row |SKU| Name               | Price  |Taxed| Qty | Min |   Total    |Atn\n");
+	printf("----+---+--------------------+--------+-----+-----+-----+------------|---\n");
+}
+
+void printFooter(double grandTotal)
+{
+	printf("--------------------------------------------------------+----------------\n");
+
+	if (grandTotal > 0)
+	{
+		printf("                                           Grand Total: |%12.2lf\n", grandTotal);
+	}
+}
+
+void flushKeyboard(void)
+{
+	while (getchar() != '\n');
+}
+
+void pause(void)
+{
+	printf("Press <ENTER> to continue...");
+	flushKeyboard();
+}
+
+int getInt(void)
+{
+	int flag = 0;
+	int integer = 0;
+	char newLine;
+
+	do {
+		scanf("%d%c", &integer, &newLine);
+
+		if (newLine != '\n') {
+			printf("Invalid integer, please try again: ");
+			flushKeyboard();
+		}
+		else {
+			flag = 1;
+		}
+	} while (flag != 1);
+
+	return integer;
+}
+
+int getIntLimited(int lowerLimit, int upperLimit)
+{
+	int value;
+	value = getInt();
+	while (value > upperLimit || value < lowerLimit)
+	{
+		printf("Invalid value, %d < value < %d: ", lowerLimit, upperLimit);
+		value = getInt();
+	}
+	return value;
+}
+
+double getDouble(void)
+{
+	int flag = 0;
+	double Double = 0;
+	char newLine;
+
+	do {
+		scanf("%lf%c", &Double, &newLine);
+
+		if (newLine != '\n') {
+			printf("Invalid number, please try again: ");
+			flushKeyboard();
+		}
+		else {
+			flag = 1;
+		}
+
+	} while (flag != 1);
+
+	return Double;
+}
+
+double getDoubleLimited(double lowerLimit, double upperLimit)
+{
+	double value;
+	value = getDouble();
+	while (value > upperLimit || value < lowerLimit)
+	{
+		printf("Invalid value, %lf< value < %lf: ", lowerLimit, upperLimit);
+		value = getDouble();
+	}
+	return value;
+}
+
+int yes(void)
+{
+	char YN;
+	int value;
+	scanf("%c", &YN);
+	while (YN != 'y' && YN != 'Y'&& YN != 'n' && YN != 'N')
+	{
+
+		printf("Only (Y)es or (N)o are acceptable: ");
+		flushKeyboard();
+		scanf("%c", &YN);
+	}
+	if (YN == 'Y' || YN == 'y')
+	{
+		value = 1;
+	}
+	if (YN == 'n' || YN == 'N')
+	{
+		value = 0;
+	}
+	return value;
+}
+
+int menu(void)
+{
+	int integer = 0;
+
+	printf("1- List all items\n");
+	printf("2- Search by SKU\n");
+	printf("3- Checkout an item\n");
+	printf("4- Stock an item\n");
+	printf("5- Add new item or update item\n");
+	printf("6- delete item\n");
+	printf("7- Search by name\n");
+	printf("0- Exit program\n");
+	printf("> ");
+
+	integer = getIntLimited(0, 7);
+	return integer;
+}
+void GroceryInventorySystem(void)
+{
+	int option = 0;
+	int close = 0;
+	welcome();
+
+	do {
+		option = menu();
+		switch (option)
+		{
+		case 0:
+			printf("Exit the program? (Y)es/(N)o: ");
+			if (yes() == 1)
+			{
+				close = 1;
+			}
+			break;
+		case 1:
+			printf("List Items under construction!\n");
+			pause();
+			break;
+
+		case 2:
+			printf("Search Items under construction!\n");
+			pause();
+			break;
+
+		case 3:
+			printf("Checkout Item under construction!\n");
+			pause();
+			break;
+
+		case 4:
+			printf("Stock Item under construction!\n");
+			pause();
+			break;
+
+		case 5:
+			printf("Add/Update Item under construction!\n");
+			pause();
+			break;
+
+		case 6:
+			printf("Delete Item under construction!\n");
+			pause();
+			break;
+
+		case 7:
+			printf("Search by name under construction!\n");
+			pause();
+			break;
+
+		default:
+			printf("Invalid value, 0 < value < 7: ");
+			pause();
+			break;
+		}
+	} while (close != 1);
+}
+
+// Function Implementations Milestone 2:
+double totalAfterTax(struct Item item)
+{
+	double Total = 0;
+
+	Total = item.price * item.quantity;
+	if (item.isTaxed == 1)
+	{
+		Total += (Total * TAX);
+	}
+	return Total;
+}
+
+
+int isLowQuantity(struct Item item)
+{
+	int flag = 0;
+	if (item.quantity < item.minQuantity)
+	{
+		flag = 1;    //error if the above statement is true
+	}
+	else
+	{
+		flag = 0;  //zero means false
+	}
+	return flag;
+}
+
+struct Item itemEntry(int sku)
+{
+	struct Item item;
+
+	item.sku = sku;							//sku
+	printf("        SKU: %d\n", sku);
+	
+	printf("       Name: ");				//name
+	scanf("%20[^\n]", item.name);
+	flushKeyboard();
+
+	printf("      Price: ");				//price
+	item.price = getDouble();
+
+	printf("   Quantity: ");				//quantity
+	item.quantity = getInt();
+
+	printf("Minimum Qty: ");				//minimum_quatity
+	item.minQuantity = getInt();
+
+	printf("   Is Taxed: ");				//is_tax
+	item.isTaxed = yes();
+	flushKeyboard();
+
+	return item;
+}
+
+void displayItem(struct Item item, int linear)
+{
+	//prints item out when linear is true
+	if (linear == 1)
+	{
+
+		printf("|%3d", item.sku);
+		printf("|%-20s", item.name);
+		printf("|%8.2lf", item.price);
+
+		if (item.isTaxed == 0)  //if its not taxed retrun no
+		{
+			return("|   No");
+		}
+		else
+		{
+			printf("|  Yes");
+
+		}
+
+		printf("| %3d ", item.quantity);
+
+		printf("| %3d ", item.minQuantity);
+
+		printf("|%12.2lf|", totalAfterTax(item));
+
+		if (item.quantity < item.minQuantity)
+		{
+			printf("***\n");
+		}
+
+		else
+		{
+			printf("\n");
+		}
+	}
+	//prints out item when linear is false.
+	if (linear == 0)
+	{
+		printf("        SKU: %d\n", item.sku);
+		printf("       Name: %s\n", item.name);
+		printf("      Price: %.2lf\n", item.price);
+		printf("   Quantity: %d\n", item.quantity);
+		printf("Minimum Qty: %d\n", item.minQuantity);
+		if (item.isTaxed == 0)
+		{
+			printf("   Is Taxed: No\n");
+		}
+		else
+		{
+			printf("   Is Taxed: Yes\n");
+		}
+		if (item.quantity < item.minQuantity)
+		{
+			printf("WARNING: Quantity low, please order ASAP!!!\n");
+		}
+	}
+}
+
+void listItems(const struct Item item[], int noOfItems)
+{
+	int i = 0;
+	double grandTotal = 0;
+
+	printTitle();
+	for (i = 0; i < noOfItems; i++)
+	{
+		printf("%-4d", i + 1);
+		printf("|%3d", item[i].sku);
+		printf("|%-20s", item[i].name);
+		printf("|%8.2lf", item[i].price);
+
+		if (item[i].isTaxed == 0)
+		{
+
+			printf("|   No");
+
+		}
+		else
+		{
+
+			printf("|  Yes");
+
+		}
+
+		printf("| %3d ", item[i].quantity);
+
+		printf("| %3d ", item[i].minQuantity);
+
+		printf("|%12.2lf|", totalAfterTax(item[i]));
+
+		if (item[i].quantity < item[i].minQuantity)
+		{
+
+			printf("***\n");
+
+		}
+
+		else
+		{
+
+			printf("\n");
+
+		}
+
+		grandTotal += totalAfterTax(item[i]);
+	}
+	printFooter(grandTotal);
+
+
+
+
+}
+
+int locateItem(const struct Item item[], int noOfRecs, int sku, int *index)
+{
+	int flag;
+
+
+	for (*index = 0; *index < noOfRecs; (*index)++)
+	{
+
+		if (item[*index].sku == sku)
+		{
+
+			flag = 1;
+
+		}
+		else
+		{
+			flag = 0;
+		}
+
+	}
+
+	(*index)--;
+
+	return flag;
+}
+// Function Implementations Milestone 2:
+
+
+
---
+++ Milestones 2/144_ms2_tester.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LINEAR 1
+#define FORM 0
+
+/* ms1*/
+void welcome(void);
+void printTitle(void);
+void printFooter(double gTotal);
+void pause(void);
+void flushKeyboard(void);
+int getInt(void);
+double getDouble(void);
+int getIntLimited(int lowerLimit, int upperLimit);
+double getDoubleLimited(double lowerLimit, double upperLimit);
+int yes(void);  
+
+/* ms2*/
+struct Item {
+   double price;
+   int sku;
+   int isTaxed;
+   int quantity;
+   int minQuantity;
+   char name[21];
+};
+
+
+double totalAfterTax(struct Item item);
+int isLowQuantity(struct Item item);
+struct Item itemEntry(int sku);
+void displayItem(struct Item item, int linear);
+void listItems(const struct Item item[], int NoOfItems);
+int locateItem(const struct Item item[], int NoOfRecs, int sku, int* index);
+
+
+
+
+int main() {
+   struct Item I[21] = {
+      { 4.4,275,0,10,2,"Royal Apples" },
+      { 5.99,386,0,20,4,"Watermelon" },
+      { 3.99,240,0,30,5,"Blueberries" },
+      { 10.56,916,0,20,3,"Seedless Grapes" },
+      { 2.5,385,0,5,2,"Pomegranate" },
+      { 0.44,495,0,100,30,"Banana" },
+      { 0.5,316,0,123,10,"Kiwifruit" },
+      { 4.49,355,1,20,5,"Chicken Alfredo" },
+      { 5.49,846,1,3,5,"Veal Parmigiana" },
+      { 5.29,359,1,40,5,"Beffsteak Pie" },
+      { 4.79,127,1,30,3,"Curry Checken" },
+      { 16.99,238,1,10,2,"Tide Detergent" },
+      { 10.49,324,1,40,5,"Tide Liq. Pods" },
+      { 10.99,491,1,50,5,"Tide Powder Det." },
+      { 3.69,538,1,1,5,"Lays Chips S&V" },
+      { 3.29,649,1,15,5,"Joe Org Chips" },
+      { 1.79,731,1,100,10,"Allen's Apple Juice" },
+      { 6.69,984,1,30,3,"Coke 24 Pack" },
+      { 7.29,350,1,50,5,"Nestea 24 Pack" },
+      { 6.49,835,1,20,2,"7up 24 pack" }
+   };
+   double val;
+   int ival;
+   int searchIndex;
+   val = totalAfterTax(I[0]);
+   printf("totalAfterTax:\n"
+          "    yours=%lf\n"
+          "program's=44.000000\n", val);
+   val = totalAfterTax(I[7]);
+   printf("totalAfterTax:\n"
+          "    yours=%lf\n"
+          "program's=101.474000\n", val);
+   ival = isLowQuantity(I[0]);
+   printf("isLowQuantity:\n"
+          "    yours=%d\n"
+          "program's=0\n",ival);
+   ival = isLowQuantity(I[14]);
+   printf("isLowQuantity:\n"
+          "    yours=%d\n"
+          "program's=1\n",ival);
+   pause();
+   
+   printf("itemEntry, enter the following values:\n");
+   printf("        SKU: 999\n"
+      "       Name: Red Apples\n"
+      "      Price: 4.54\n"
+      "   Quantity: 50\n"
+      "Minimum Qty: 5\n"
+      "   Is Taxed: n\n");
+   printf("Enter the values:\n");
+   I[20] = itemEntry(999);
+   printf("displayItem, Linear:\nYours: ");
+   displayItem(I[20], LINEAR);
+   printf(" Prog: |999|Red Apples          |    4.54|   No|  50 |   5 |      227.00|\n");
+   printf("displayItem, Form:\nYours:\n");
+   displayItem(I[20], FORM);
+   printf("Programs: \n");
+   printf("        SKU: 999\n"
+      "       Name: Red Apples\n"
+      "      Price: 4.54\n"
+      "   Quantity: 50\n"
+      "Minimum Qty: 5\n"
+      "   Is Taxed: No\n");
+   I[20].quantity = 2;
+   I[20].isTaxed = 1;
+   pause();
+   
+   printf("displayItem, Linear with low value and taxed:\nYours: ");
+   displayItem(I[20], LINEAR);
+   printf(" Prog: |999|Red Apples          |    4.54|  Yes|   2 |   5 |       10.26|***\n");
+   printf("displayItem, Form with low value:\nYours:\n");
+   displayItem(I[20], FORM);
+   printf("Programs: \n");
+   printf("        SKU: 999\n"
+      "       Name: Red Apples\n"
+      "      Price: 4.54\n"
+      "   Quantity: 2\n"
+      "Minimum Qty: 5\n"
+      "   Is Taxed: Yes\n"
+      "WARNING: Quantity low, please order ASAP!!!\n");
+   pause();
+   
+   printf("Listing Items: \n");
+   listItems(I, 21);
+   pause();
+   printf("Locate Item, successful search:\n");
+   printf("program: Found the item 999 at index: 20\n");
+   printf("  Yours: ");
+   if (locateItem(I, 21, 999, &searchIndex)) {
+      printf("Found the item 999 at index: %d\n", searchIndex);
+   }
+   else {
+      printf("No item with the sku 999 is in the array!\n");
+   }
+
+   printf("Locae Item, unsuccessful search:\n");
+   printf("Progam: No item with the sku 732 is in the array!\n");
+   printf(" Yours: ");
+   if (locateItem(I, 21, 732, &searchIndex)) {
+      printf("Found the item at index: %d\n", searchIndex);
+   }
+   else {
+      printf("No item with the sku 732 is in the array!\n");
+   }
+   return 0;
+}
+
+
